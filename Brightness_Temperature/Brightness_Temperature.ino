96d5b4feda6ad0d0a67131cb59a9594d528f6f7f
Dopamine81 <mohammadjreza81@gmail.com>
2025-07-13 10:48:20 +0330

    Running sht20

--- Brightness_Temperature/Brightness_Temperature.ino
+++ Brightness_Temperature/Brightness_Temperature.ino
@@ -3,7 +3,10 @@ DFRobot_SHT20 sht20(&Wire, SHT20_I2C_ADDR);
 
 void setup() {
   // put your setup code here, to run once:
-
+  sht20.initSHT20();
+  delay(100);
+  sht20.checkSHT20();
+  
 }
 
 void loop() {
